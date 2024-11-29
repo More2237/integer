91683ecf32d95ba78f92596fbdd92264a851059f
More2237 <noorallyrahmonov2308@gmail.com>
2024-11-29 10:08:24 +0500

    idgfg

---
+++ integer1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace integer1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var L = double.Parse(Console.ReadLine());
+            var result = L / 100;
+            Console.WriteLine(result+"  metr");
+        }
+    }
+}
